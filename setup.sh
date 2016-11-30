8af8b669f82f8f523b57be5b8bc5e50a3c09b121
Yamatomo <mazda13bturbo@gmail.com>
2016-11-30 16:23:04 +0900

    * ファイル名の修正  * setup.shの改修

--- setup.sh
+++ setup.sh
@@ -4,20 +4,33 @@ dest=~/
 current=$(cd $(dirname $0);pwd)
 source=$current/repos
 
-for file in $(ls $source -a )
+files=$(ls $source)
+if [ $# -ne 0 ]; then
+  files="$@"
+fi
+
+for file in $files
 do
+  file=`basename $file`
   if [ "$file" = "." -o "$file" = ".." ]; then
     continue
   fi
 
+  if [ ! -f "$source/$file" ]; then
+    continue
+  fi
+  
+  #echo $file
+  #continue
+
   if [ ! -L $dest/$file ]; then
     echo "######### $file ###########"
 
-    if [ -f ~/$file ]; then
-      cp ~/$file $current/org
-      rm ~/$file
+    if [ -f ~/.$file ]; then
+      cp ~/.$file $current/org/$file
+      rm ~/.$file
     fi
-    ln -s $source/$file $dest/$file
+    ln -s $source/$file $dest/.$file
   fi
 done
 
